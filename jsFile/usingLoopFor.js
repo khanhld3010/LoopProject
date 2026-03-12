55425000d9c3db720c802103a05447144aafa4f3
khanhld3010 <luuduckhanh3010@gmail.com>
2026-03-12 15:49:56 +0700

    using loop for

---
+++ jsFile/usingLoopFor.js
@@ -0,0 +1,11 @@
+// let i;
+// for (i = 0; i <=5; i++) {
+//     console.log(i);
+// }
+
+let num = prompt("Enter your number: ");
+let sum = 0;
+for (let i = 1; i <= num; i++) {
+    sum += i;
+    console.log(sum);
+}
